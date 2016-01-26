f54115a0bb720a7288f1ebe3d4c7a3f12086168b
riverlight <heliang1108@vip.sina.com>
2016-01-26 15:21:03 +0800

    release v 0.02 1. add kafka support (run in spark cluster env)
    Signed-off-by: riverlight <heliang1108@vip.sina.com>

--- mfs/src/main/scala/com/leon/mfs.scala
+++ mfs/src/main/scala/com/leon/mfs.scala
@@ -5,6 +5,7 @@ import com.mongodb.casbah.{MongoCredential, MongoClient}
 import com.mongodb.casbah.commons.MongoDBObject
 import org.apache.spark.streaming.flume._
 import org.apache.spark.streaming.StreamingContext
+import org.apache.spark.streaming.kafka.KafkaUtils
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.streaming.Seconds
 import org.apache.spark.storage.StorageLevel
@@ -23,10 +24,14 @@ object mfs {
   private var dbCollName : String = "mysca";
   private var flHostname : String = "localhost";
   private var flPort : Int = 11114;
+  var zkQuorum = "10.10.69.166:2181";
+  var consumerGroup = "test-consumers";
+  var topics = "test";
 
   def getConfig(confFile : String) : Unit = {
     for (line <- Source.fromFile(confFile).getLines()) {
       val keyVal = line.split("=")
+      println(keyVal(0))
       keyVal(0) match {
         case "dbHost" => dbHost = keyVal(1);
         case "dbPort" => dbPort = keyVal(1).toInt;
@@ -36,6 +41,9 @@ object mfs {
         case "dbCollName" => dbCollName = keyVal(1);
         case "flHostname" => flHostname = keyVal(1);
         case "flPort" => flPort = keyVal(1).toInt;
+        case "zkQuorum" => zkQuorum = keyVal(1);
+        case "consumerGroup" => consumerGroup = keyVal(1);
+        case "topics" => topics = keyVal(1);
       }
     }
   }
@@ -49,7 +57,7 @@ object mfs {
 
     if (args.length < 1) {
       print("please enter config file")
-      //System.exit(1)
+      System.exit(1)
     }
     getConfig(args(0))
     printConf()
@@ -66,15 +74,21 @@ object mfs {
     val ssc = new StreamingContext(sc, Seconds(20))
 
     val storageLevel = StorageLevel.MEMORY_ONLY
+    val consumerThreadsPerInputStream = 1
 
-    println(flHostname + "  " + flPort)
-    val flumeStream = FlumeUtils.createPollingStream(ssc, flHostname, flPort, storageLevel)
-    flumeStream.foreachRDD( rdd => {
-      //rdd.count().map( cnt => "Received " + cnt + " flume events." ).print()
-      print(rdd.count().toString())
-      val count1 = MongoDBObject("count" -> rdd.count())
-      mysca.insert(count1)
-    })
+    println("\n" + zkQuorum + " " + consumerGroup)
+    val topicMap = topics.split(",").map((_, 1.toInt)).toMap
+    val kafkaStream = KafkaUtils.createStream(ssc, zkQuorum, consumerGroup, topicMap);
+    kafkaStream.count().map(cnt => "Received " + cnt + " events.\n").print()
+
+//    println(flHostname + "  " + flPort)
+//    val flumeStream = FlumeUtils.createPollingStream(ssc, flHostname, flPort, storageLevel)
+//    flumeStream.foreachRDD( rdd => {
+//      //rdd.count().map( cnt => "Received " + cnt + " flume events." ).print()
+//      print(rdd.count().toString())
+//      val count1 = MongoDBObject("count" -> rdd.count())
+//      mysca.insert(count1)
+//    })
 
     ssc.start()
     //计算完毕退出
